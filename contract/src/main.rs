5db62726bc92fcf2863371128779f04b08ff08aa
Jiuhong <jiuhong@casperlabs.io>
2021-11-25 11:18:01 +0800

    first commit

---
+++ contract/src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+#[cfg(not(target_arch = "wasm32"))]
+compile_error!("target arch should be wasm32: compile with '--target wasm32-unknown-unknown'");
+
+// We need to explicitly import the std alloc crate and `alloc::string::String` as we're in a
+// `no_std` environment.
+extern crate alloc;
+
+use alloc::string::String;
+
+use casper_contract::{
+    contract_api::{runtime, storage},
+    unwrap_or_revert::UnwrapOrRevert,
+};
+use casper_types::{ApiError, Key};
+
+#[no_mangle]
+pub extern "C" fn call() {
+    let named_keys = runtime::list_named_keys();
+    for (key,_) in named_keys {
+        if key.starts_with("cep47-nft-2") {
+            runtime::remove_key(&key);
+        }
+    }
+    
+}
